a0ee7da105874a0e6d566c9e5f4c1060ff93b165
tejal-1303 <79006362+tejal-1303@users.noreply.github.com>
2022-11-12 02:26:56 +0530

    executing api implementation

--- server/src/routes/api-routes.ts
+++ server/src/routes/api-routes.ts
@@ -7,7 +7,8 @@ import { scrapeQuestion } from "../utils/scrapeQuestion";
 
 const router = Router();
 
-
+// we will do our re-routing from the client side just send information from here
+// GET to /api/auth will return current logged in user info
 router.get("/auth", (req, res) => {
   if (!req.user) {
     return res.status(200).json({
@@ -17,14 +18,14 @@ router.get("/auth", (req, res) => {
         name: "",
         image_link: "",
       },
-      
+      // cookies: undefined,
     });
   } else {
     return res.status(200).json({
       isLoggedIn: true,
       message: "User is logged in",
       user: req.user,
-      
+      // cookies: req.cookies,
     });
   }
 });
@@ -35,18 +36,41 @@ router.get("/logout", (req, res) => {
   res.send({ message: "Successfully logged out" });
 });
 
-router.post("/fetch-problems", async (req, res) => {
-  const { tags, difficulty, companies } = req.body as Record<string, string[]>;
-  res.json(await filterQuestions(tags, difficulty, companies));
+router.post("/execute", async (req, res) => {
+  const { script, language, stdin } = req.body;
+  if (!language || !script) {
+    return res.status(200).json({
+      message: "Code should not be empty and Language undefined",
+    });
+  }
+
+  const response = await axios({
+    method: "POST",
+    url: `${JDOODLE_URL}/execute`,
+    data: {
+      script: script,
+      stdin: stdin,
+      language: getLanguage[language],
+      versionIndex: getLanguageVersion[language],
+      clientId: JDOODLE.clientID,
+      clientSecret: JDOODLE.clientSecret,
+    },
+    responseType: "json",
+  });
+  res.json(response.data);
 });
 
-router.post("/fetch-contest-problem", async (req, res) => {
-  const { url, hostname } = req.body as Record<string, string>;
-  const questionData = await scrapeQuestion(url, hostname);
-  res.json(questionData);
+router.get("/credit-spent", async (req, res) => {
+  const response = await axios({
+    method: "POST",
+    url: `${JDOODLE_URL}/credit-spent`,
+    data: {
+      clientId: JDOODLE.clientID,
+      clientSecret: JDOODLE.clientSecret,
+    },
+    responseType: "json",
+  });
+  res.json(response.data);
 });
 
-router.post("/get-problem", async (req, res) => {
-  const { question_id } = req.body as Record<string, string>;
-  res.json(await renderQuestion(question_id));
-});
+export default router;
