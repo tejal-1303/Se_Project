fa0ad565c7317770feb16ed67b2bfcf37e1e6608
ranjithcoder <74724775+ranjithcoder@users.noreply.github.com>
2022-11-12 10:47:08 +0530

    configured react-router-dom in client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 const App = () => {
-  return <div>React app</div>;
+  const Sample = lazy(() => import('./pages/Sample/SamplePage'));
+
+  return (
+    <div style={{ height: 'auto' }}>
+      <Router>
+        <Suspense fallback={<h1>Loading..</h1>}>
+          <Switch>
+            <Route path="/sample" component={Sample} />
+          </Switch>
+        </Suspense>
+      </Router>
+    </div>
+  );
 };
 
 export default App;
